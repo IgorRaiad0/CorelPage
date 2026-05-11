dbd5ce84427852ccff84fb2e070c3ca9e83fce7c
Igor Raiado <igorsonic56@gmail.com>
2026-05-11 15:11:35 -0300

    Fix vídeo aula 04

--- js/aulas.js
+++ js/aulas.js
@@ -37,7 +37,7 @@ thumb:"thumbs/Aula03.jpeg"
 id:4,
 titulo:"(Aula 04) Tipografia",
 liberada:true,
-video:"https://www.youtube.com/watch?v=yyjUmv1gJEg&list=RDyyjUmv1gJEg&start_radio=1",
+video:"https://youtu.be/djfcSIuDo1s",
 slide:"2PACX-1vR9DJ8RlXXdEciIbGp-AhQnSf8KVQjtaQkJbOX_a3J5tbVrOB2RXPUgIvY5zf0lA-9hGfmyLlEAqKWm",
 temArquivos:true,
 arquivos:"https://drive.google.com/drive/folders/1Si3nBSIZPTj0zIITIGdlO39pACm14Owi?usp=sharing",
